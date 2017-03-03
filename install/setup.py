edb4c831af73994a3b41d5fc67270c12ca54beb0
Shubham Yadav <shubhamy2015@gmail.com>
2017-03-03 14:54:33 +0530

    updating setup file

--- install/setup.py
+++ install/setup.py
@@ -2,28 +2,44 @@ import subprocess
 import os
 from os.path import dirname, realpath, pardir, join, sep
 
-###################################################
-# Adding psylab to PYTHONPATH in .bashrc
-###################################################
+################################################
+# Adding psylab to PYTHONPATH in .bashrc, .zshrc
+################################################
 
 HOME_PATH = os.getenv('HOME')
 BASHRC = HOME_PATH + sep + '.bashrc'
+ZSHRC = HOME_PATH + sep + '.zshrc'
 PSYLAB_DIR = join(dirname(realpath(__file__)), pardir)
 
 export_command = "export PYTHONPATH=$PYTHONPATH:%s" % (PSYLAB_DIR, )
 
 isExport = False
-with open(BASHRC, mode='r') as bashrc_file:
-    lines = bashrc_file.readlines()
-    for line in lines:
-        if 'psylab' in line:
-            isExport = True
-bashrc_file.close()
 
-if not isExport:
-    # .bashrc will be created if it does not exist
-    e = "echo %s >> %s" % (export_command, BASHRC, )
-    subprocess.call(e, shell=True)
+if os.path.exists(BASHRC):
+    with open(BASHRC, mode='r') as bashrc_file:
+        lines = bashrc_file.readlines()
+        for line in lines:
+            if 'psylab' in line:
+                isExport = True
+    bashrc_file.close()
+
+    if not isExport:
+        # .bashrc will be created if it does not exist
+        e = "echo %s >> %s" % (export_command, BASHRC, )
+        subprocess.call(e, shell=True)
+
+if os.path.exists(ZSHRC):
+    with open(ZSHRC, mode='r') as zshrc_file:
+        lines = zshrc_file.readlines()
+        for line in lines:
+            if 'psylab' in line:
+                isExport = True
+    zshrc_file.close()
+
+    if not isExport:
+        # .zshrc will be created if it does not exist
+        e = "echo %s >> %s" % (export_command, ZSHRC, )
+        subprocess.call(e, shell=True)
 
 ###################################################
 # Installing dependencies
